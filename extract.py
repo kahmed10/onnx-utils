f979dcc88540e48e6f2652acfb01e78d8022235a
Umang Yadav <umayadav@amd.com>
2024-03-01 20:53:22 +0000

    add argparser

--- extract.py
+++ extract.py
@@ -9,7 +9,7 @@ import onnx.checker
 import onnx.helper
 import onnx.shape_inference
 from onnx import FunctionProto, ModelProto, NodeProto, TensorProto, ValueInfoProto
-from onnx.external_data_helper import load_external_data_for_model
+from onnx.external_data_helper import convert_model_to_external_data, load_external_data_for_model
 
 
 class Extractor:
@@ -182,7 +182,7 @@ class Extractor:
 
 
 def extract_model(
-    input_path: str | os.PathLike,
+    model: ModelProto,
     output_path: str | os.PathLike,
     input_names: list[str],
     output_names: list[str],
@@ -203,17 +203,10 @@ def extract_model(
         output_names (list of string): The names of the output tensors that to be extracted.
         check_model (bool): Whether to run model checker on the extracted model.
     """
-    if not os.path.exists(input_path):
-        raise ValueError(f"Invalid input model path: {input_path}")
     if not output_path:
         raise ValueError("Output model path shall not be empty!")
     if not output_names:
         raise ValueError("Output tensor names shall not be empty!")
-    onnx.checker.check_model(input_path)
-    model = onnx.load(input_path, load_external_data=False)
-    model = onnx.shape_inference.infer_shapes(model)
-    load_external_data_for_model(
-        model, os.path.dirname(input_path))
 
     e = Extractor(model)
     extracted = e.extract_model(input_names, output_names)
--- graph.py
+++ graph.py
@@ -1,42 +1,63 @@
 from extract import extract_model
+import argparse
 import os
 import onnx
 import sys
 import onnx.utils
 from onnx.external_data_helper import convert_model_to_external_data, load_external_data_for_model
 
-if len(sys.argv) != 4:
-    print(
-        "Usage: python graph.py [/path/to/model.onnx] [path/to/subgraphx.onnx] [num_layers]")
-    exit()
 
-onnx_model = sys.argv[1]
-subgraphx_path = sys.argv[2]
-num_layers = int(sys.argv[3])
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="get first N layers from an onnx model")
+    parser.add_argument('--input-path', type=str,
+                        dest='input_path', help='path to input onnx file', required=True)
+    parser.add_argument('--output-path', type=str,
+                        dest='output_path', help='path to output onnx file', required=True)
+    parser.add_argument('--num-layers', type=int,  dest='num_layers',
+                        help='Number of layers to chop from the top', required=True)
+    parser.add_argument('--shape-infer', dest="shape_infer", default=False,
+                        action='store_true', help='Flag to whether run shape inference or not')
+    parser.add_argument('--external-data', dest='external_data', default=False,
+                        action='store_true', help='Whether the model has any external data or not')
+    args = parser.parse_args()
+    return args, parser
 
-model = onnx.load(onnx_model, load_external_data=False)
-model = onnx.shape_inference.infer_shapes(model)
-load_external_data_for_model(
-    model, os.path.dirname(onnx_model))
-# Then the onnx_model has loaded the external data from the specific directory
-# Then the onnx_model has converted raw data as external data and saved to specific directory
-model_input_names = [i.name for i in model.graph.input]
-model_output_names = [i.name for i in model.graph.output]
-model_weights_names = [i.name for i in model.graph.initializer]
-export_input_names = []
-export_output_names = []
-for node in model.graph.node[0:num_layers]:
-    for i in node.input:
-        if (i in model_input_names) or (i in model_weights_names):
-            if i not in export_input_names:
-                export_input_names.append(i)
-        if i in export_output_names:
-            export_output_names.remove(i)
-    for o in node.output:
-        export_output_names.append(o)
 
-print(export_input_names)
-print(export_output_names)
+if __name__ == "__main__":
+    args, parser = parse_args()
+    onnx_model = args.input_path
+    if not os.path.exists(onnx_model):
+        raise ValueError(f"Invalid input model path: {onnx_model}")
 
-extract_model(onnx_model, subgraphx_path,
-              export_input_names, export_output_names)
+    output_path = args.output_path
+    num_layers = args.num_layers
+    external_data = args.external_data
+    onnx.checker.check_model(onnx_model)
+    model = onnx.load(onnx_model, load_external_data=False)
+    if args.shape_infer:
+        model = onnx.shape_inference.infer_shapes(model)
+    if args.external_data:
+        load_external_data_for_model(model, os.path.dirname(onnx_model))
+    model_input_names = [i.name for i in model.graph.input]
+    model_output_names = [i.name for i in model.graph.output]
+    model_weights_names = [i.name for i in model.graph.initializer]
+    export_input_names = []
+    export_output_names = []
+    for node in model.graph.node[0:num_layers]:
+        for i in node.input:
+            if (i in model_input_names) or (i in model_weights_names):
+                if i not in export_input_names:
+                    export_input_names.append(i)
+            if i in export_output_names:
+                export_output_names.remove(i)
+        for o in node.output:
+            export_output_names.append(o)
+    print("model has total: " + str(len(model.graph.node)) +
+          " layers and out of that extracting the first " + str(num_layers) + " layers")
+    print("inputs to the extracted models are: ", export_input_names)
+    print("outputs of the extracted models are: ", export_output_names)
+
+    extract_model(model, output_path,
+                  export_input_names, export_output_names)
+
